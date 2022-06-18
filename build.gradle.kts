cdee5d6713a1381c94904993c858fedde55bd9b2
SouravKB <souravkannanthab3219@gmail.com>
2022-06-18 13:53:06 +0530

    Updated gradle; Added Firebase, Navigation-component, Jetpack-compose.

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    // NOTE: Do not place your application dependencies here; they belong
+    // in the individual module build.gradle files
+    dependencies {
+
+        val agpVersion = "7.2.1"
+        classpath("com.android.tools.build:gradle:$agpVersion")
+
+        val kotlinVersion = "1.7.0"
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+
+        val navVersion = "2.4.2"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+
+        val googleServicesVersion = "4.3.10"
+        classpath("com.google.gms:google-services:$googleServicesVersion")
+
+        val crashlyticsVersion = "2.8.1"
+        classpath("com.google.firebase:firebase-crashlytics-gradle:$crashlyticsVersion")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
