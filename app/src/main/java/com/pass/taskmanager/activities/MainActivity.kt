beef6fb785becca0667cba92560ffcad07c72e14
SouravKB <souravkannanthab3219@gmail.com>
2022-06-18 15:02:28 +0530

    Starter code

---
+++ app/src/main/java/com/pass/taskmanager/activities/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pass.taskmanager.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
